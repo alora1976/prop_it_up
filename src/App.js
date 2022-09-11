635c6f25bb067774c702165f6126d4f8c1ec25b2
Lori Trindade <39773598+alora1976@users.noreply.github.com>
2022-09-11 15:32:17 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
-import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import PersonCard from "./components/PersonCard";
+
+const peopleArr = [
+  {
+    firstName: "Joe",
+    lastName: "Smith",
+    age: 28,
+    hairColor: "blonde",
+  },
+  {
+    firstName: "Matilda",
+    lastName: "Jones",
+    age: 33,
+    hairColor: "brown",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Johnson",
+    age: 34,
+    hairColor: "blonde",
+  },
+  {
+    firstName: "Hal",
+    lastName: "Moore",
+    age: 90,
+    hairColor: "grey",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <PersonCard
-        lastName={"Smith"}
-        firstName={"John"}
-        age={25}
-        hair={"Red"}
+      {peopleArr.map((personObj, index) => (
+        <PersonCard
+          key={index}
+          firstName={personObj.firstName}
+          lastName={personObj.lastName}
+          age={personObj.age}
+          hairColor={personObj.hairColor}
+        />
+      ))}
+      {/* <PersonCard
+        firstName={peopleArr[0].firstName}
+        lastName={peopleArr[0].lastName}
+        age={peopleArr[0].age}
+        hairColor={peopleArr[0].hairColor}
       />
       <PersonCard
-        lastName={"Smith"}
-        firstName={"Anna"}
-        age={45}
-        hair={"Blue"}
+        firstName={peopleArr[1].firstName}
+        lastName={peopleArr[1].lastName}
+        age={peopleArr[1].age}
+        hairColor={peopleArr[1].hairColor}
       />
-      <PersonCard lastName={"Lopez"} firstName={"Jen"} age={25} hair={"purple"} />
       <PersonCard
-        lastName={"JOhnson"}
-        firstName={"Matt"}
-        age={50}
-        hair={"green"}
+        firstName={peopleArr[2].firstName}
+        lastName={peopleArr[2].lastName}
+        age={peopleArr[2].age}
+        hairColor={peopleArr[2].hairColor}
       />
+      <PersonCard
+        firstName={peopleArr[3].firstName}
+        lastName={peopleArr[3].lastName}
+        age={peopleArr[3].age}
+        hairColor={peopleArr[3].hairColor}
+      /> */}
     </div>
   );
 }
